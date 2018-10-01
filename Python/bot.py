cda25fc9ac8aa630c6ecb27302a7a28b6adb2ed8
Namenot <Namenot@users.noreply.github.com>
2018-10-02 01:14:56 +0200

    mostly done
    missing a check for double nominations
    missing a check for multiple votes by a single person
    missing an ImDB search

--- Python/bot.py
+++ Python/bot.py
@@ -6,20 +6,22 @@ from discord.ext.commands import has_permissions, MissingPermissions
 
 token = "NDk1OTUxMTU0NjczNjE0ODQ4.DpJiyQ.5NgmMJVfjshxVLt0eBna4ndVVt0"
 
-bot = commands.Bot(command_prefix = '>', description='A Bot that manages a voting system for a movie night.')
+bot = commands.Bot(command_prefix = 'b!', description='A Bot that manages a voting system for a movie night.')
 
 voting = []
 votes = []
 vote = 0
 
 def checkspaces(a):
-    nos = 10 #number of spaces
+    nos = 30 #number of spaces
     nos -= len(a)
-
     if nos >=0:
-        while 
-
-
+        strg = a
+        for a in range(nos+5):
+            strg += " "
+    else:
+        strg = a[:nos]
+        strg += "...  "
     return strg
 
 
@@ -32,55 +34,78 @@ async def on_ready():
     print(bot.user.id)
     print("--------------")
 
-@bot.event
-async def on_message(msg):
+@bot.command()
+async def nominate(ctx, *,msg: str):
     global voting
     global votes
-    if msg.content.startswith('>bnominate'):
-        voting.append(msg[11:]) ##check the IMdB database
+    if vote:
+        voting.append(msg) ##check the IMdB database
         votes.append(0)
-
+        print(voting)
+        strg  = "``` You nominated "
+        strg += msg
+        strg += "```"
+        await ctx.send(strg)
+    else:
+        await ctx.send("```Nominting a movie is not possible at the moment```\n")
 
 @bot.command(pass_context=True)
 @has_permissions(administrator=True)
 async def start(ctx):
     global vote
     vote = 1
-    await ctx.send("`Voting has begun`")
+    await ctx.send("```Voting has begun```")
 
 
 @bot.command(pass_context=True)
 @has_permissions(administrator=True)
 async def stop(ctx):
     global vote
+    global voting
+    global votes
     vote = 0
-    await ctx.send("`Voting has ended`")
+    await ctx.send("```Voting has ended```")
+    #determin the winner
+    votes, voting = zip(*sorted(zip(votes, voting)))
+    print(voting)
+    strg  = "```The winner is...\n"
+    strg += voting[len(voting)-1]
+    strg += "\nwith "
+    strg += str(votes[len(votes)-1])
+    strg += " votes```\n"
+    await ctx.send(strg)
+
+
 
 @bot.command()
 async def votefor(ctx, a: int):
+    global votes
     if vote:
-        strg  = "`You voted for "
+        #votes[len(votes)-(a+1)] +=1
+        votes[a-1] +=1
+        strg  = "```You voted for "
+        #strg += voting[len(voting)-(a+1)]
         strg += voting[a-1]
-        strg += "`"
+        strg += "```"
         await ctx.send(strg)
     else:
-        await ctx.send("`Voting is not in progress`")
+        await ctx.send("```Voting is not in progress```")
     #here I still need a system to track all votes
 
 @bot.command()
 async def bhelp(ctx):
     strg  = "```The following commands are currently available:\n"
     strg += "----------------------------------------------\n"
-    strg += "bstart    : starts the voting process(**_ADMIN_** only)\n"
-    strg += "bstop     : stops the voting process(**_ADMIN_** only)\n"
+    strg += "start    : starts the voting process(**_ADMIN_** only)\n"
+    strg += "stop     : stops the voting process(**_ADMIN_** only)\n"
     strg += "binfo     : gives you details about the current vote in progress\n"
-    strg += "bvotefor  : let's you vote for a nominated Movie\n"
-    strg += "bnominate : let's you nominate a Movie\n"
+    strg += "votefor  : let's you vote for a nominated Movie\n"
+    strg += "nominate : let's you nominate a Movie\n"
     strg += "bhelp     : DUH\n```"
     await ctx.send(strg)
 
 @bot.command()
-async def info(ctx):
+async def binfo(ctx):
     i = 0
     if len(voting) > 0:
         strg = "```"
@@ -90,20 +115,24 @@ async def info(ctx):
             strg += "["
             strg += str(i+1)
             strg += "] "
-            strg += voting[len(voting)-(i+1)]
-            strg += "                      "##add function that finds the necessary number of spaces
-            strg += str(votes[len(votes)-(i+1)])
+            #strg += voting[len(voting)-(i+1)]
+            #strg += "                      "##add function that finds the necessary number of spaces
+            #strg += checkspaces(voting[len(voting)-(i+1)])
+            #strg += str(votes[len(votes)-(i+1)])
+            strg += checkspaces(voting[i])
+            strg += str(votes[i])
             strg += " votes\n"
             i+= 1
 
         strg += "```"
         await ctx.send(strg)
     else:
-        await ctx.send("`Voting is not in progress`")
+        await ctx.send("```Voting is not in progress```")
 
 bot.run(token)
 
 
-#misses a fuction were one can nominate a Movie
-#misses a fuction that can search a movie data base, to check whether the
+#misses a function that checks for double nominations
+#misses a function that regulates, that everyone can vote multiple times
+#misses a function that can search a movie data base, to check whether the
 #       nominated movie exists
---
+++ Python/test.py
@@ -0,0 +1,15 @@
+def checkspaces(a):
+    nos = 30 #number of spaces
+    nos -= len(a)
+    if nos >=0:
+        strg = a
+        for a in range(nos+5):
+            strg += " "
+    else:
+        strg = a[:nos]
+        strg += "...  "
+    return strg
+
+a = "Hallo Darkness my old friend, I've come to talk with you again"
+b = checkspaces(a)
+print(b)
