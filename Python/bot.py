01beb3cf8370464ca3fc9434ea303aea28cffcb6
Namenot <Namenot@users.noreply.github.com>
2018-10-01 23:22:39 +0200

    added alot of functions

--- Python/bot.py
+++ Python/bot.py
@@ -1,9 +1,16 @@
 import discord
 from discord.ext import commands
 
+from discord import Member
+from discord.ext.commands import has_permissions, MissingPermissions
+
 token = "NDk1OTUxMTU0NjczNjE0ODQ4.DpJiyQ.5NgmMJVfjshxVLt0eBna4ndVVt0"
 
-bot = commands.Bot(command_prefix = 'b!', description='A Bot that manages a voting system for a movie night.')
+bot = commands.Bot(command_prefix = '>', description='A Bot that manages a voting system for a movie night.')
+
+voting = ['Movie 1', 'Movie 2', 'Movie 3']
+votes = [3, 10, 50]
+vote = 0
 
 @bot.event
 async def on_ready():
@@ -13,29 +20,71 @@ async def on_ready():
     print(bot.user.id)
     print("--------------")
 
-@bot.command()
-async def add(ctx, a: int, b: int):
-    await ctx.send(a+b)
+@bot.command(pass_context=True)
+@has_permissions(administrator=True)
+async def start(ctx):
+    global vote
+    vote = 1
+    await ctx.send("`Voting has begun`")
+
+
+@bot.command(pass_context=True)
+@has_permissions(administrator=True)
+async def stop(ctx):
+    global vote
+    vote = 0
+    await ctx.send("`Voting has ended`")
 
 @bot.command()
-async def multiply(ctx, a: int, b: int):
-    await ctx.send(a*b)
+async def votefor(ctx, a: int):
+    if vote:
+        strg  = "`You voted for "
+        strg += voting[a-1]
+        strg += "`"
+        await ctx.send(strg)
+    else:
+        await ctx.send("`Voting is not in progress`")
+    #here I still need a system to track all votes
 
 @bot.command()
-async def greet(ctx):
-    await ctx.send(":smiley: :wave: Hello, there!")
+async def bhelp(ctx):
+    strg  = "```The following commands are currently available:\n"
+    strg += "----------------------------------------------\n"
+    strg += "bstart    : starts the voting process(**_ADMIN_** only)\n"
+    strg += "bstop     : stops the voting process(**_ADMIN_** only)\n"
+    strg += "binfo     : gives you details about the current vote in progress\n"
+    strg += "bvotefor  : let's you vote for a nominated Movie\n"
+    strg += "bnominate : let's you nominate a Movie\n"
+    strg += "bhelp     : DUH\n```"
+    await ctx.send(strg)
+
+@bot.
 
 @bot.command()
-async def cat(ctx):
-    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+async def info(ctx):
+    i = 0
+    if len(voting) > 0:
+        strg = "```"
+        strg += "\n These Movies are currently up for voting:"
+        strg += "\n ------------------------------------------\n"
+        while i < len(voting):
+            strg += "["
+            strg += str(i+1)
+            strg += "] "
+            strg += voting[len(voting)-(i+1)]
+            strg += "                      "##add function that finds the necessary number of spaces
+            strg += str(votes[len(votes)-(i+1)])
+            strg += " votes\n"
+            i+= 1
 
-@bot.command()
-async def printer(ctx, a: str, b: str):
-    a += ""
-    b += ""
-    c += ""
-    c = a + b
-    await ctx.send(c)
+        strg += "```"
+        await ctx.send(strg)
+    else:
+        await ctx.send("`Voting is not in progress`")
 
 bot.run(token)
-print("Done")
+
+
+#misses a fuction were one can nominate a Movie
+#misses a fuction that can search a movie data base, to check whether the
+#       nominated movie exists
